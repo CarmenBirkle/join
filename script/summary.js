dbb3bd9f3e49a7d1202ef9bb3ca0ae45ebe62f33
Daniel Hartmann <126328974+DHartmann94@users.noreply.github.com>
2023-03-23 19:16:01 +0100

    summaryGreeting function

---
+++ script/summary.js
@@ -0,0 +1,15 @@
+function summaryGreetings() {
+    let currentHour = new Date().getHours();
+
+    let greeting;
+
+    if(currentHour < 12) {
+        greeting = "Good morning,"
+    } else if (currentHour < 18) {
+        greeting = "Good afternoon,"
+    } else {
+        greeting = "Good evening,"
+    }
+
+    document.getElementById('summary-greeting').innerHTML = greeting;
+}
