e27d5f73a0088fdd8665d21e54522a1c5b199299
Daniel Hartmann <126328974+DHartmann94@users.noreply.github.com>
2023-03-24 19:41:07 +0100

    add task category js

--- script/add-task.js
+++ script/add-task.js
@@ -1,5 +1,36 @@
-let addTaskCategorys = [];
+let allCategoryColor = ['#FC71FF', '#1FD7C1' ,'#FF8A00', '#8AA4FF', '#FF0000', '#2AD300', '#E200BE', '#0038FF'];
+let defaultCategoryColor = ['#FC71FF','#1FD7C1', '#FF8A00', '#8AA4FF'];
+let defaultCategoryType = ['Sale', 'Backoffice', 'Design', 'Marketing'];
 
-function renderCategoryDropdown() {
-    let dopdownOpen = document.getElementById('add-task-category-dropdown-open');
+
+function openCategoryDropdown() {
+    document.getElementById('add-task-category-dropdown').classList.toggle('d-none');
+    renderCategorySelection();
+}
+
+function renderCategorySelection() {
+    document.getElementById('add-task-category-dropdown').innerHTML = '';
+    document.getElementById('add-task-category-dropdown').innerHTML = openNewCategory();
+
+    for(let i=0; i < defaultCategoryColor.length; i++) {
+        let color = defaultCategoryColor[i];
+        let type = defaultCategoryType[i];
+
+        document.getElementById('add-task-category-dropdown').innerHTML += openCategorysHTML(color, type);
+    }
+}
+
+function openNewCategory() {
+    return`
+    <div class="add-task-dropdown-new-category">New Category</div>
+    `;
+}
+
+function openCategorysHTML(color, type) {
+    return `
+    <div class="add-task-dropdown-option">
+        <div>${type}</div>
+        <div class="dopdown-option-dots" style="background-color: ${color};"></div>
+    </div>
+     `;
 }
