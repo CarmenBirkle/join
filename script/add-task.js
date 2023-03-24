09f0a11ecea56b061bb828790472126e0ba64e1f
Daniel Hartmann <126328974+DHartmann94@users.noreply.github.com>
2023-03-24 21:37:53 +0100

    add task category dropdown

--- script/add-task.js
+++ script/add-task.js
@@ -5,32 +5,61 @@ let defaultCategoryType = ['Sale', 'Backoffice', 'Design', 'Marketing'];
 
 function openCategoryDropdown() {
     document.getElementById('add-task-category-dropdown').classList.toggle('d-none');
+    renderTopCategory();
     renderCategorySelection();
+}
+
+function renderTopCategory() {
+    document.getElementById('add-task-category-dropdown-top').innerHTML = '';
+    document.getElementById('add-task-category-dropdown-top').innerHTML = openTopCategoryPlaceholderHTML();
 }
 
 function renderCategorySelection() {
     document.getElementById('add-task-category-dropdown').innerHTML = '';
-    document.getElementById('add-task-category-dropdown').innerHTML = openNewCategory();
+    document.getElementById('add-task-category-dropdown').innerHTML = openNewCategoryHTML();
 
-    for(let i=0; i < defaultCategoryColor.length; i++) {
-        let color = defaultCategoryColor[i];
-        let type = defaultCategoryType[i];
+    for(let j=0; j < defaultCategoryColor.length; j++) {
+        let color = defaultCategoryColor[j];
+        let type = defaultCategoryType[j];
 
         document.getElementById('add-task-category-dropdown').innerHTML += openCategorysHTML(color, type);
     }
 }
 
-function openNewCategory() {
+function setCategory(color, type) {
+    document.getElementById('add-task-category-dropdown-top').innerHTML = openTopSetCategory(color, type);
+    document.getElementById('add-task-category-dropdown').innerHTML = '';
+    document.getElementById('add-task-category-dropdown').classList.add('d-none');
+}
+
+function openTopCategoryPlaceholderHTML() {
     return`
-    <div class="add-task-dropdown-new-category">New Category</div>
+    <span>Select task category</span>
+    <img src="assets/img/icons/add-task-dropdown-arrow.svg" alt="arrow">
+    `;
+}
+
+function openNewCategoryHTML() {
+    return`
+    <span class="add-task-dropdown-new-category">New Category</span>
     `;
 }
 
 function openCategorysHTML(color, type) {
     return `
-    <div class="add-task-dropdown-option">
-        <div>${type}</div>
+    <div class="add-task-dropdown-option" onclick="setCategory('${color}', '${type}')">
+        <span>${type}</span>
         <div class="dopdown-option-dots" style="background-color: ${color};"></div>
     </div>
+     `;
+}
+
+function openTopSetCategory(color, type) { // add-task-dropdown-option-selected ohne hover und padding
+    return `
+    <div class="add-task-dropdown-option">
+        <span>${type}</span>
+        <div class="dopdown-option-dots" style="background-color: ${color};"></div>
+    </div>
+    <img src="assets/img/icons/add-task-dropdown-arrow.svg" alt="arrow">
      `;
 }
