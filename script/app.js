5f601183ca611dd0b02dfb5eed618ace1f542b5e
Carmen Birkle <102891110+CarmenBirkle@users.noreply.github.com>
2023-03-24 11:09:45 +0100

    begin with menue logic for bottons

--- script/app.js
+++ script/app.js
@@ -1,10 +1,12 @@
-// main java-script file 
+// main java-script file , side behavior an all pages, menue
+
+let currentSide = 'side-bar-add-Task'; // global variable to set the current page
 
 async function init() {
     await includeHTML();
 }
 
-async function includeHTML() {
+async function includeHTML(currentPage) {
     let includeElements = document.querySelectorAll('[w3-include-html]');
     for (let i = 0; i < includeElements.length; i++) {
         const element = includeElements[i];
@@ -12,9 +14,22 @@ async function includeHTML() {
         let resp = await fetch(file);
         if (resp.ok) {
             element.innerHTML = await resp.text();
+            getElement();
         } else {
             element.innerHTML = 'Page not found';
         }
+    }
+
+}
+//noch in Arbei -> Carmen
+// open a page, the side-bar menu becomes active (dark-blue botton)
+function getElement() {
+    console.log('geladen'); // kann raus wenns funzt
+    let currentSideElement = document.getElementById(currentSide);
+    if (currentSideElement !== null) {
+        console.log(currentSideElement); // kann raus wenns funzt
+        console.log('geklappt'); // kann raus wenns funzt
+        currentSideElement.classList.add('side-bar-position');
     }
 }
 
