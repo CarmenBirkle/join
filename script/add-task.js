1d85a2debe334dd017f92c73d185e3467d8dd9d6
Daniel Hartmann <126328974+DHartmann94@users.noreply.github.com>
2023-03-26 10:14:48 +0200

    add error message

--- script/add-task.js
+++ script/add-task.js
@@ -1,11 +1,14 @@
-let allCategoryColor = ['#FC71FF', '#1FD7C1' ,'#FF8A00', '#8AA4FF', '#FF0000', '#2AD300', '#E200BE', '#0038FF'];
-let defaultCategoryColor = ['#FC71FF','#1FD7C1', '#FF8A00', '#8AA4FF'];
+const allCategoryColor = ['#FC71FF', '#1FD7C1', '#FF8A00', '#8AA4FF', '#FF0000', '#2AD300', '#E200BE', '#0038FF'];
+let defaultCategoryColor = ['#FC71FF', '#1FD7C1', '#FF8A00', '#8AA4FF'];
 let defaultCategoryType = ['Sale', 'Backoffice', 'Design', 'Marketing'];
+let selectedColorNewCategory = [];
 
 
+/*-- Category --*/
 function initCategory() {
     document.getElementById('add-task-category-render').innerHTML = loadCategoryHTML();
     document.getElementById('add-task-new-category-dots').innerHTML = '';
+    document.getElementById('add-task-new-category-error').innerHTML = '';
 }
 
 function openCategoryDropdown() {
@@ -23,7 +26,7 @@ function renderCategorySelection() {
     document.getElementById('add-task-category-dropdown').innerHTML = '';
     document.getElementById('add-task-category-dropdown').innerHTML = openNewCategoryHTML();
 
-    for(let j=0; j < defaultCategoryColor.length; j++) {
+    for (let j = 0; j < defaultCategoryColor.length; j++) {
         let color = defaultCategoryColor[j];
         let type = defaultCategoryType[j];
 
@@ -40,19 +43,41 @@ function setCategory(color, type) {
 function renderNewCategory() {
     document.getElementById('add-task-category-render').innerHTML = '';
     document.getElementById('add-task-category-render').innerHTML = openNewCategorySelectHTML();
-    renderNewCategoryDots()
+    renderNewCategoryDots();
+    renderNewCategoryError();
 }
 
 function renderNewCategoryDots() {
     document.getElementById('add-task-new-category-dots').innerHTML = '';
 
-    for(let d=0; d < allCategoryColor.length; d++) {
+    for (let d = 0; d < allCategoryColor.length; d++) {
         let dotColor = allCategoryColor[d];
         document.getElementById('add-task-new-category-dots').innerHTML += openNewCategoryDotsHTML(dotColor);
     }
 }
 
-/*-- Wenn man eine neue Kategorie erstellt ins default array reinpushen????? --*/
+/*-- Category add new Category --*/
+function renderNewCategoryError() {
+    document.getElementById('add-task-new-category-error').innerHTML = '';
+    document.getElementById('add-task-new-category-error').innerHTML = newCategoryErrorHTML();
+}
+
+function saveNewColor(dotColor) {
+    selectedColorNewCategory = [];
+    selectedColorNewCategory.push(dotColor);
+    console.log(selectedColorNewCategory); // TEST: WENN FUNKTION FERTIG DANN LÖSCHEN
+}
+
+function saveNewCategory() {
+    let newType = document.getElementById('new-category-type-name');
+
+    if (newType.value == '' || selectedColorNewCategory.length == 0) {
+        console.log('ERROR KEIN TYPE ODER FARBE');
+        const errorMessage = document.querySelector('.new-category-error');
+        errorMessage.classList.add('d-flex');
+        return;
+    }
+}
 
 
 
@@ -103,11 +128,11 @@ function openTopSetCategoryHTML(color, type) {
 function openNewCategorySelectHTML() {
     return /*html*/`
     <div class="add-task-category-dropdown-top">
-        <input class="add-task-new-categroy-input" type="text" placeholder="New category name">
+        <input id="new-category-type-name" class="add-task-new-categroy-input" type="text" placeholder="New category name">
             <div class="add-task-new-categroy-buttons">
             <img src="./assets/img/icons/add-task-button-cross.svg" onclick="initCategory()" alt="cross">
             <div class="add-task-category-greyline"></div>
-            <img src="./assets/img/icons/add-task-button-check.svg" alt="check">
+            <img src="./assets/img/icons/add-task-button-check.svg" onclick="saveNewCategory()" alt="check">
         </div>
     </div>
     `;
@@ -115,6 +140,12 @@ function openNewCategorySelectHTML() {
 
 function openNewCategoryDotsHTML(dotColor) {
     return /*html*/`
-    <div class="dropdown-option-dots new-category-dot" style="background-color: ${dotColor};"></div>
+    <div class="dropdown-option-dots new-category-dot" style="background-color: ${dotColor};" onclick="saveNewColor('${dotColor}')"></div>
+    `;
+}
+
+function newCategoryErrorHTML() {
+    return /*html*/ `
+    <span class="new-category-error">Please write a category name and pick a color</span>
     `;
 }
