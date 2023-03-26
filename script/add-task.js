c333e98078ba1019394fe05c4e4555b51f9e2dcf
Daniel Hartmann <126328974+DHartmann94@users.noreply.github.com>
2023-03-26 10:42:45 +0200

    complete new category

--- script/add-task.js
+++ script/add-task.js
@@ -44,7 +44,6 @@ function renderNewCategory() {
     document.getElementById('add-task-category-render').innerHTML = '';
     document.getElementById('add-task-category-render').innerHTML = openNewCategorySelectHTML();
     renderNewCategoryDots();
-    renderNewCategoryError();
 }
 
 function renderNewCategoryDots() {
@@ -57,26 +56,35 @@ function renderNewCategoryDots() {
 }
 
 /*-- Category add new Category --*/
-function renderNewCategoryError() {
-    document.getElementById('add-task-new-category-error').innerHTML = '';
-    document.getElementById('add-task-new-category-error').innerHTML = newCategoryErrorHTML();
-}
-
 function saveNewColor(dotColor) {
     selectedColorNewCategory = [];
     selectedColorNewCategory.push(dotColor);
-    console.log(selectedColorNewCategory); // TEST: WENN FUNKTION FERTIG DANN LÖSCHEN
 }
 
 function saveNewCategory() {
     let newType = document.getElementById('new-category-type-name');
 
     if (newType.value == '' || selectedColorNewCategory.length == 0) {
-        console.log('ERROR KEIN TYPE ODER FARBE');
-        const errorMessage = document.querySelector('.new-category-error');
-        errorMessage.classList.add('d-flex');
-        return;
+        renderNewCategoryError();
+    } else {
+        defaultCategoryColor = defaultCategoryColor.concat(selectedColorNewCategory);
+        defaultCategoryType.push(newType.value);
+        initCategory();
+        addedNewCategoryMessage();
     }
+}
+
+function renderNewCategoryError() {
+    document.getElementById('add-task-new-category-error').innerHTML = '';
+    document.getElementById('add-task-new-category-error').innerHTML = newCategoryErrorHTML();
+}
+
+function addedNewCategoryMessage() {
+    document.getElementById('add-task-new-category-error').innerHTML = '';
+    document.getElementById('add-task-new-category-error').innerHTML = newCategoryAddedHTML();
+    setTimeout(() => {
+        document.getElementById('add-task-new-category-error').innerHTML = '';
+    }, 2000)
 }
 
 
@@ -147,5 +155,11 @@ function openNewCategoryDotsHTML(dotColor) {
 function newCategoryErrorHTML() {
     return /*html*/ `
     <span class="new-category-error">Please write a category name and pick a color</span>
+    `;
+}
+
+function newCategoryAddedHTML() {
+    return /*html*/ `
+    <span class="new-category-error">Added new category</span>
     `;
 }
