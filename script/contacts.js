719fcd99a6242518cd46f22dcf1bced501c7bbe3
Carmen Birkle <102891110+CarmenBirkle@users.noreply.github.com>
2023-03-28 09:00:31 +0200

    overlay new Contact html done

---
+++ script/contacts.js
@@ -0,0 +1,11 @@
+function contactsShowOverlayNew(){
+    let overlay = document.getElementById('contacts-popup-add-contact');
+    overlay.style.display = "flex";
+}
+
+function contactsCloseOverlayNew(){ 
+    let overlay = document.getElementById('contacts-popup-add-contact');
+    overlay.style.display = "none";
+}
+       
+ 
