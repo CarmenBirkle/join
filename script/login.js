bb197fd8e28273c186c071c4dd45480db9a982d0
PascalGajewski <pascal.gajewski77@gmail.com>
2023-03-28 19:17:29 +0200

    started boatd html

--- script/login.js
+++ script/login.js
@@ -4,14 +4,14 @@
 function startAnimation(){
     if(screen.width >= 768){
     document.getElementById('login-logo').classList.add('animation');
-    setTimeout(showLogin, 3000);
+    setTimeout(showLoginCard, 3000);
     }
     if(screen.width < 768){
         responsiveLogin();
     };
 }
 
-function showLogin(){
+function showLoginCard(){
     document.getElementById('login-content').classList.remove('d-none');
     document.getElementById('login-animation-container').classList.add('d-none');
 }
@@ -20,7 +20,7 @@ function responsiveLogin(){
     document.getElementById('login-logo').classList.add('d-none');
     document.getElementById('login-logo-responsive').classList.remove('d-none');
     document.getElementById('login-logo-responsive').classList.add('animation');
-    setTimeout(showLogin, 3000);
+    setTimeout(showLoginCard, 3000);
 }
 
 /**
