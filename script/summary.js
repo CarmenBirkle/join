932983d9e6fcf5ba86c62846afe62932ac33a43b
Daniel Hartmann <126328974+DHartmann94@users.noreply.github.com>
2023-04-04 15:11:09 +0200

    countUrgent / findDeadline

--- script/summary.js
+++ script/summary.js
@@ -6,7 +6,7 @@ function summaryGreetings() {
     let currentHour = new Date().getHours();
     let greeting;
 
-    if(currentHour < 12) {
+    if (currentHour < 12) {
         greeting = "Good morning,"
     } else if (currentHour < 18) {
         greeting = "Good afternoon,"
@@ -26,17 +26,59 @@ function summaryGreetings() {
 
 function countUrgent() {
     let urgentCount = 0;
-    for (let i = 0; i < users.length; i++) {
-        let userTasks = users[i].tasks;
-        for (let j = 0; j < userTasks.length; j++) {
-            if (userTasks[j].prio === 'Urgent') {
-                urgentCount++;
-            }
+    for (let i = 0; i < testTasks.length; i++) {
+        let taskPrio = testTasks[i].prio;
+        if (taskPrio === 'Urgent') {
+            urgentCount++;
         }
     }
     console.log('Anzahl der Aufgaben mit prio "Urgent": ' + urgentCount);
 }
 
 function findDeadline() {
-
+    let lowestDate = Infinity;
+    for (let i = 0; i < testTasks.length; i++) {
+        let milliseconds = testTasks[i].date;
+        if (milliseconds < lowestDate) {
+            lowestDate = milliseconds;
+        }
+    }
+    const date = new Date(lowestDate);
+    const formattedDate = date.toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' });
+    console.log(formattedDate);
 }
+
+let testTasks = [{
+    'number': 1,
+    'title': 'Call potential clients',
+    'description': 'Make the product presentation to prospective buyers',
+    'categoryColor': '#FC71FF',
+    'categoryType': 'Sales',
+    'category': 'to-do',
+    'contact': ['David Eisenberg', 'Benedikt Ziegler', 'Marcel Bauer', 'Stefanie Farber'],
+    'date': 1367644800000,
+    'prio': 'Urgent',
+    'subtask': ''
+}, {
+    'number': 2,
+    'title': 'Call potential clients',
+    'description': 'Make the product presentation to prospective buyers',
+    'categoryColor': '#FC71FF',
+    'categoryType': 'Sales',
+    'category': 'in-progress',
+    'contact': ['David Eisenberg', 'Benedikt Ziegler', 'Marcel Bauer', 'Stefanie Farber'],
+    'date': 1667644800000,
+    'prio': 'Medium',
+    'subtask': ''
+}, {
+    'number': 2,
+    'title': 'Call potential clients',
+    'description': 'Make the product presentation to prospective buyers',
+    'categoryColor': '#FC71FF',
+    'categoryType': 'Sales',
+    'category': 'in-progress',
+    'contact': ['David Eisenberg', 'Benedikt Ziegler', 'Marcel Bauer', 'Stefanie Farber'],
+    'date': 1167644800000,
+    'prio': 'Urgent',
+    'subtask': ''
+} ]
