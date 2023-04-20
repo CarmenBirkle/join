d146b9a45605fe66ec73dc65c70ab933c2c730c4
PascalGajewski <121040841+PascalGajewski@users.noreply.github.com>
2023-04-20 18:57:13 +0200

    splitted the board js into 3 files

---
+++ script/board-template-add-task.js
@@ -0,0 +1,17 @@
+/**
+ *  This function closes a opened Add Task Popup
+ */
+
+function closeAddTaskPopup() {
+    document.getElementById('board-add-task').classList.add('d-none');
+    window.location.reload();
+}
+
+/**
+ * This function closes the add tasks popup
+ */
+
+function boardCloseAddTask() {
+    document.getElementById('board-add-task').classList.add('d-none');
+    document.getElementById('board-content').classList.remove('d-none');
+}
